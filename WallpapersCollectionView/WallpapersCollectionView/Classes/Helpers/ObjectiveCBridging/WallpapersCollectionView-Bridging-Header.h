56923f04b590c5d4c3d1bb6ad5a414ad0ab2f33e
Bogdan Iusco <bogdan.iusco@gmail.com>
2014-08-09 14:58:51 +0300

    Integrate pods.

---
+++ WallpapersCollectionView/WallpapersCollectionView/Classes/Helpers/ObjectiveCBridging/WallpapersCollectionView-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  WallpapersCollectionView-Bridging-Header.h
+//  WallpapersCollectionView
+//
+//  Created by Bogdan Iusco on 8/9/14.
+//  Copyright (c) 2014 Grigaci. All rights reserved.
+//
+
+#import <CJALog/CJALog.h>
+#import <CJAMacros/CJAMacros.h>
+#import <MagicalRecord/MagicalRecord.h>
